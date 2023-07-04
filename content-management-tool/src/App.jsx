85a858b90e39237e92b4c28738f440347e4701aa
Jayanthbabu456 <babujayanth456@gmail.com>
2023-07-04 20:13:58 +0530

    localstorage added

--- content-management-tool/src/App.jsx
+++ content-management-tool/src/App.jsx
@@ -1,66 +1,4 @@
-// import { useState, useEffect } from "react";
-// import { Route, Routes, useParams } from "react-router-dom";
-// import Main from "./components/Main";
-// import Header from "./components/Header";
-// import AllBlogs from "./components/AllBlogs";
-// import BlogForm from "./components/BlogForm";
-// import Blog from "./components/Blog";
-// import { v4 as uuid } from "uuid";
-
-// function App() {
-//   const [blogData, setBlogData] = useState([]);
-//   useEffect(() => {
-//     const storedBlogs = localStorage.getItem("blogs");
-//     if (storedBlogs) {
-//       setBlogData(JSON.parse(storedBlogs));
-//     }
-//   }, []);
-
-//   const handleFormSubmit = (formData) => {
-//     const isEmpty = Object.values(formData).some((value) => value === "");
-//     if (isEmpty) {
-//       alert("All fields are required");
-//       return;
-//     }
-//     const blogWithId = { ...formData, id: uuid() };
-//     setBlogData((prevData) => [...prevData, blogWithId]);
-//   };
-//   useEffect(() => {
-//     localStorage.setItem("blogs", JSON.stringify(blogData));
-//   }, [blogData]);
-
-//   return (
-//     <div className="relative h-screen w-full gradient overflow-hidden flex justify-center items-center">
-//       <div className="absolute left-[-220px] top-[-100px] w-[550px] h-[550px] rounded-full opacity-40 bg-gradient-to-r from-[#ff9a9e] to-[#f6416c]"></div>
-
-//       <div className="absolute bottom-[-100px] right-[-220px] w-[550px] h-[550px] rounded-full opacity-40  bg-gradient-to-r from-[#ff9a9e] to-[#f6416c]"></div>
-//       <div className="design">
-//         <Header />
-//         <Routes>
-//           <Route path="/" element={<Main />} />
-//           <Route path="/Allblogs" element={<AllBlogs blogs={blogData} />} />
-//           <Route
-//             path="/Writeblog"
-//             element={<BlogForm onFormSubmit={handleFormSubmit} />}
-//           />
-//           <Route path="/blog/:id" element={<BlogPage blogs={blogData} />} />
-//         </Routes>
-//       </div>
-//     </div>
-//   );
-// }
-
-// function BlogPage({ blogs }) {
-//   const { id } = useParams();
-
-//   const blog = blogs.find((blog) => blog.id === id);
-
-//   return <Blog blog={blog} />;
-// }
-
-// export default App;
-
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Route, Routes, useParams } from "react-router-dom";
 import Main from "./components/Main";
 import Header from "./components/Header";
@@ -70,9 +8,13 @@ import Blog from "./components/Blog";
 import { v4 as uuid } from "uuid";
 
 function App() {
-  const [blogData, setBlogData] = useState([]);
+  const [blogData, setBlogData] = useState(() => {
+    const storedBlogs = localStorage.getItem("blogs");
+    return storedBlogs ? JSON.parse(storedBlogs) : [];
+  });
 
-  const handleFormSubmit = (formData) => {
+  const handleFormSubmit = (event, formData) => {
+    event.preventDefault();
     const isEmpty = Object.values(formData).some((value) => value === "");
     if (isEmpty) {
       alert("All fields are required");
@@ -81,6 +23,10 @@ function App() {
     const blogWithId = { ...formData, id: uuid() };
     setBlogData((prevData) => [...prevData, blogWithId]);
   };
+
+  useEffect(() => {
+    localStorage.setItem("blogs", JSON.stringify(blogData));
+  }, [blogData]);
 
   return (
     <div className="relative h-screen w-full gradient overflow-hidden flex justify-center items-center">
@@ -91,7 +37,10 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Main />} />
-          <Route path="/Allblogs" element={<AllBlogs blogs={blogData} />} />
+          <Route
+            path="/Allblogs"
+            element={<AllBlogs blogs={blogData} setBlogData={setBlogData} />}
+          />
           <Route
             path="/Writeblog"
             element={<BlogForm onFormSubmit={handleFormSubmit} />}
--- content-management-tool/src/components/AllBlogs.jsx
+++ content-management-tool/src/components/AllBlogs.jsx
@@ -1,48 +1,6 @@
-// import Card from "./Card";
-
-// const AllBlogs = ({ blogs }) => {
-//   return (
-//     <div className="w-[88%] mx-auto">
-//       <div>
-//         <p className="text-4xl font-montserrat font-semibold text-[#fff] text-center">
-//           All Blogs
-//         </p>
-//       </div>
-//       {blogs.length === 0 ? (
-//         <p className="text-2xl font-montserrat font-semibold text-[#fff] text-center mt-5 h-[50vh] flex flex-col justify-center">
-//           No blogs available
-//         </p>
-//       ) : (
-//         <div className="flex flex-wrap gap-[20px]  overflow-scroll scroll h-[65vh] py-[30px] justify-center">
-//           {blogs.map((blog, index) => (
-//             <Card key={index} blog={blog} />
-//           ))}
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default AllBlogs;
-
 import Card from "./Card";
 
-const AllBlogs = ({ blogs }) => {
-  if (blogs.length === 0) {
-    return (
-      <div className="w-[88%] mx-auto">
-        <div>
-          <p className="text-4xl font-montserrat font-semibold text-[#fff] text-center">
-            All Blogs
-          </p>
-        </div>
-        <p className="text-2xl font-montserrat font-semibold text-[#fff] text-center mt-5 h-[50vh] flex flex-col justify-center">
-          No blogs available
-        </p>
-      </div>
-    );
-  }
-
+const AllBlogs = ({ blogs, setBlogData }) => {
   return (
     <div className="w-[88%] mx-auto">
       <div>
@@ -50,11 +8,17 @@ const AllBlogs = ({ blogs }) => {
           All Blogs
         </p>
       </div>
-      <div className="flex flex-wrap gap-[20px]  overflow-scroll scroll h-[65vh] py-[30px] justify-center">
-        {blogs.map((blog, index) => (
-          <Card key={index} blog={blog} />
-        ))}
-      </div>
+      {blogs.length === 0 ? (
+        <p className="text-2xl font-montserrat font-semibold text-[#fff] text-center mt-5 h-[50vh] flex flex-col justify-center">
+          No blogs available
+        </p>
+      ) : (
+        <div className="flex flex-wrap gap-[20px]  overflow-scroll scroll h-[65vh] py-[30px] justify-center">
+          {blogs.map((blog, index) => (
+            <Card key={index} blog={blog} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
