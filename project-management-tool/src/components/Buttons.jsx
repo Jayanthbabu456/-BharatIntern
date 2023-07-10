4dbf36aa675dd1fa4f382246431c6752b9159b09
Mohandeep <mohandeep2002@gmail.com>
2023-07-10 22:21:58 +0530

    Wrong Spelling 'CANCEL'

--- project-management-tool/src/components/Buttons.jsx
+++ project-management-tool/src/components/Buttons.jsx
@@ -1,37 +1,37 @@
 import { toast } from "react-toastify";
 
 const Buttons = ({
-  handleSave,
-  setTitle,
-  setDescription,
-  title,
-  description,
+    handleSave,
+    setTitle,
+    setDescription,
+    title,
+    description,
 }) => {
-  const handleCancle = () => {
-    if (title === "" && description === "") {
-      alert("Fields are Emppty");
-      return;
-    }
-    setTitle("");
-    setDescription("");
-    toast.warn("Task cancelled successfully!");
-  };
-  return (
-    <div className="flex flex-row gap-4 pt-5">
-      <button
-        className="py-[15px] px-[20px] text-[16px] font-bold rounded-md bg-[#f1af71] shadow-current font-montserrat"
-        onClick={handleSave}
-      >
-        SAVE
-      </button>
-      <button
-        className="py-[15px] px-[20px] text-[16px] font-bold rounded-md bg-[#f1af71] shadow-current font-montserrat"
-        onClick={handleCancle}
-      >
-        CANCLE
-      </button>
-    </div>
-  );
+    const handleCancle = () => {
+        if (title === "" && description === "") {
+            alert("Fields are Emppty");
+            return;
+        }
+        setTitle("");
+        setDescription("");
+        toast.warn("Task cancelled successfully!");
+    };
+    return (
+        <div className="flex flex-row gap-4 pt-5">
+            <button
+                className="py-[15px] px-[20px] text-[16px] font-bold rounded-md bg-[#f1af71] shadow-current font-montserrat"
+                onClick={handleSave}
+            >
+                SAVE
+            </button>
+            <button
+                className="py-[15px] px-[20px] text-[16px] font-bold rounded-md bg-[#f1af71] shadow-current font-montserrat"
+                onClick={handleCancle}
+            >
+                CANCEL
+            </button>
+        </div>
+    );
 };
 
 export default Buttons;
